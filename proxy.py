00bc9cf7914cad960bd259d8fed9889921509ef2
nvssks <n.vassakis@gmail.com>
2016-10-07 15:51:53 +0100

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -60,6 +60,8 @@ def main():
 	parser.add_option('-b','--buffer', help='HTTP request size (some webshels have limitations on the size)', dest='bufferSize', action='store', type='int', default=Defaults['bufferSize'])
 	advancedGroup.add_option('-q','--ping-interval', help='webshprx pinging thread interval (default = 0.5)', dest='ping_delay', action='store', type='float', default=Defaults['ping_delay'])
 	advancedGroup.add_option('-s','--start-ping', help='Start the pinging thread first - some services send data first (eg. SSH)', dest='start_p_thread', action='store_true', default=Defaults['start_p_thread'])
+	advancedGroup.add_option('-c','--verify-server-cert', help='Verify Server Certificate', dest='start_p_thread', action='store_false', default=Defaults['ignoreServerCert'])
+
 	parser.add_option_group(advancedGroup)
 
 	(args, opts) = parser.parse_args()
